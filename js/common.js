78c295eccde2343735854026c2e867b5358c7388
trungnghia112 <trungnghia112@gmail.com>
2015-02-27 09:00:11 +0700

    update font size 14, add zoom in js

--- js/common.js
+++ js/common.js
@@ -1,5 +1,26 @@
 // JavaScript Document
 $(function() {
+	
+	var portraitWidth,landscapeWidth;
+	$(window).bind("resize", function(){
+		if(Math.abs(window.orientation) === 0){
+			if(/Android/.test(window.navigator.userAgent)){
+				if(!portraitWidth)portraitWidth=$(window).width();
+			}else{
+				portraitWidth=$(window).width();
+			}
+			$("html").css("zoom" , portraitWidth/640 );
+		}else{
+			if(/Android/.test(window.navigator.userAgent)){
+				if(!landscapeWidth)landscapeWidth=$(window).width();
+			}else{
+				landscapeWidth=$(window).width();
+			}
+			$("html").css("zoom" , landscapeWidth/640 );
+		}
+	}).trigger("resize");
+	
+	
 	$('.tn_btn_maxheight').on('click',function(){
 		var mh_father = $(this).parent( ".tn_box_maxheight" )
 		if(mh_father.hasClass('open')){
