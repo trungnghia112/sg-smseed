bb6f8d8db722330cce47a060fba10331acfa43ef
trungnghia112 <trungnghia112@gmail.com>
2015-03-10 11:38:33 +0700

    update More Less Content
    change js and css file

--- js/common.js
+++ js/common.js
@@ -1,15 +1,19 @@
 // JavaScript Document
 $(function() {
-	$('.tn_btn_maxheight').on('click',function(){
-		var mh_father = $(this).parent( ".tn_box_maxheight" )
-		if(mh_father.hasClass('open')){
-			mh_father.removeClass('open');
-			$(this).html('...続き&raquo;');
-		}else{
-			mh_father.addClass('open');
-			$(this).html('閉じる');
-		}		
+	//More-Less Text
+	$(".maxheight_show").append("<span class='maxheight_btn'>...続き&raquo;</span>");
+	$(".maxheight_hide").append("<span class='maxheight_btn'>閉じる</span>");
+	$(".maxheight_show .maxheight_btn").on('click',function(){
+		$(this).parent().next(".maxheight_hide").slideDown();
+		$(this).hide();
 	});
+	$(".maxheight_hide .maxheight_btn").on('click',function(){
+		$(this).parent().slideUp(function(){
+			$(this).prev(".maxheight_show").children(".maxheight_btn").show();
+		});
+	});
+	
+	//Accordion
 	$('.acco_box .acco_a').on("click",function(){
 		$(this).toggleClass('opened');
 		$(this).next('.acco_dv').slideToggle('slow');
